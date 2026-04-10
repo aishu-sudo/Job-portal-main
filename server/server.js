edbe529e81f686ebdc824fe4242ddb3746e32ba7
aishu-sudo <aishoozaman@example.com>
2026-04-11 01:17:57 +0600

    connection db in server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const jobRoutes = require('./routes/job');
 const paymentRoutes = require('./routes/payment');
-const path = require('path'); // Import path for resolving file locations
+const path = require('path');
+const getConnection = require('./models/db');
+
 const app = express();
 
 app.use(express.json());
 app.use('/api/jobs', jobRoutes);
 app.use('/api/payments', paymentRoutes);
 
-// Serve static files (HTML, CSS, JS) from the root and public folder
+// Serve static files
 app.use(express.static(__dirname));
 app.use(express.static('client'));
 
@@ -17,6 +19,23 @@ app.get('/freelancerDashboard.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'freelancerDashboard.html'));
 });
 
+
+//  TEST DATABASE CONNECTION 
+async function testDB() {
+    try {
+        const conn = await getConnection();
+        const result = await conn.execute("SELECT * FROM Users");
+
+        console.log("DB Connected ✅");
+        console.log(result.rows);
+
+        await conn.close();
+    } catch (err) {
+        console.error("DB Error ❌", err);
+    }
+}
+
+testDB();
 
 
 // Start the server
